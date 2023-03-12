53ad2f2ad45ffba5922e8b152a872ff979fa9b91
George Hu <georgehu@microsoft.com>
2023-03-12 12:58:58 -0700

    fix zepplin 101

--- installzepplin.ps1
+++ installzepplin.ps1
@@ -1,8 +1,18 @@
 https://pypi.org/project/airflow-providers-zeppelin/
 cp -R /mnt/c/linux/spark-3.3.1-bin-hadoop3 /mnt/c/Users/georgehu/llm/zeppelin_airflow/
 
+https://sysadminxpert.com/how-to-upgrade-java-8-to-java-11-on-ubuntu-20/
+sudo apt install openjdk-11-jdk
+sudo update-alternatives --config java
+
+Add below line in ~/.bashrc
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
+source ~/.bashrc
+
+echo $JAVA_HOME
 
 pip install airflow-providers-zeppelin
+pip install apache-airflow-providers-http
 # Successfully installed airflow-providers-zeppelin-0.0.1
 
 Starting zeppelin_airflow_redis_1    ... done
@@ -14,6 +24,12 @@ Starting zeppelin_airflow_airflow-worker_1    ... done
 Starting zeppelin_airflow_flower_1            ... done
 Starting zeppelin_airflow_airflow-scheduler_1 ... done
 Starting zeppelin_airflow_airflow-triggerer_1 ... done
+
+airflow-init_1       | /home/airflow/.local/lib/python3.7/site-packages/sqlalchemy/dialects/postgresql/base.py:3573 SAWarning: Predicate of partial index idx_dag_run_running_dags ignored during reflection
+airflow-init_1       | DB: postgresql+psycopg2://airflow:***@postgres/airflow
+airflow-init_1       | [2023-03-12 17:09:01,138] {db.py:919} INFO - Creating tables
+airflow-init_1       | INFO  [alembic.runtime.migration] Context impl PostgresqlImpl.
+airflow-init_1       | INFO  [alembic.runtime.migration] Will assume transactional DDL.
 
 
 
